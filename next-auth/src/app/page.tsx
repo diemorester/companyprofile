09849a7c4cb9535c6a224f15dee089f73621eabf
diemorester <heimrband@gmail.com>
2024-07-17 17:22:21 +0700

    Delete next-auth directory

--- next-auth/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthPage } from "@/components/auth";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <AuthPage />
-    </main>
-  );
-}
